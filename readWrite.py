0fdb77047e0f4573d976e5eca4f5f25227656e7c
Ruslan Shamshurov <ruslan.mzd@gmail.com>
2024-08-23 22:43:18 +0300

    Pupupupu

--- readWrite.py
+++ readWrite.py
@@ -22,3 +22,18 @@ output_image=open("output_image.jpg",'wb')
 output_image.write(content)
 input_file.close()
 output_image.close()
+
+class Test:
+    def __init__(self,name:str,age:int):
+        self.name = name
+        self.age = age
+        self.drop_value="Drop data"
+    def __reduce__(self):
+        return (self.__class__,(self.name,self.age))
+    def __repr__(self):
+        return f"{self.name} , {self.age}"
+
+test = Test("Ruslan",31)
+serialize_obj = pickle.dumps(test)
+deserialize_obj=pickle.loads(serialize_obj)
+print(deserialize_obj)
